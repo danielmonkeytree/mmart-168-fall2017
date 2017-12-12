81e69544872c91138ec05cddc5036b7f66efa1b8
Daniel Lippincott <31415474+danielmonkeytree@users.noreply.github.com>
2017-12-11 20:24:34 -0800

    Add files via upload

--- MatchMaker-Final 2/js/main1.js
+++ MatchMaker-Final 2/js/main1.js
@@ -1,38 +1,87 @@
+let searchTerm = null
 const sheets = new Sheets({
     key: '1Gd_Badxt9Ej6EbA5oieQbeKkwR1Yy1VI0roC-iVSs_g'
   })
 
+/* original database
+Drunken Monkey: production, composition, remixing:::oakland, CA:::
+Abba: harmonies:::sweden:::
+Justin Bieber: looking good:::Los angeles:::
+Bjork: performance art:::Iceland:::
+Devo: Dance Beats:::Ohio:::
+Robert Zimmerman: writing lyrics:::Minnesota:::
+Jimi Hendrix: guitar shredding:::London:::
+All Night Prescription: fusing genres:::portland:::
+Godfrey Daniel: composing:::Oakland:::
+*/
+
+const skillsFilter = (row) => {
+    return (row.skills.toLowerCase().search(searchTerm.toLowerCase()) !== -1)
+}
+
+const userFilter = (row) => {
+    return (row.user.toLowerCase().search(searchTerm.toLowerCase()) !== -1)
+}
+
+const projectTypeFilter = (row) => {
+    return (row.project_type.toLowerCase().search(searchTerm.toLowerCase()) !== -1)
+}
+
+const seekingFilter = (row) => {
+    return (row.seeking.toLowerCase().search(searchTerm.toLowerCase()) !== -1)
+}
+
+const locationFilter = (row) => {
+    return (row.location.toLowerCase().search(searchTerm.toLowerCase()) !== -1)
+}
+
+const anyFilter = (row) => {
+    console.log(row)
+    return skillsFilter(row) || userFilter(row) || seekingFilter(row) ||
+        locationFilter(row) || projectTypeFilter(row)
+}
 
 const doMatch = () => {
+    document.getElementById('results').innerHTML = ''
     sheets.getData(rows => {
-        rows.forEach((row) => {
+        searchTerm = document.getElementById('term').value.toLowerCase()
+        const matches = rows.filter(anyFilter)
+
+        matches.forEach((row) => {
             console.log(row)
             const li = document.createElement('li')
-            li.innerHTML = row.user + ': ' + row.skills + ':::' + row.location + ':::'
+            li.innerHTML = row.user + ': ' + row.skills + ':::' + row.location + ':::' +
+                           row.project_type + ':::' + row.seeking
+
             document.getElementById('results').appendChild(li)
         })
     })
 }
-    //filter function now needed to only return arrays that include the search 'term'
-//
-// const term = document.getElementById('term').value
-//
-//
-//     const userMatch= (term) => {
-//         /*
-//         If the string contains search term, it should return the string.
-//         If it doesn't contain search term, it should return nothing
-//         */
-//         if (doMatch.indexOf('term') >= 0) {
-//             return term
-//         } else {
-//             console.log ()
-//         }
-//     }
-// }
+
 
 //
+// const sheets = new Sheets({
+//     key: '1Gd_Badxt9Ej6EbA5oieQbeKkwR1Yy1VI0roC-iVSs_g'
+//   })
 //
+//
+// const doMatch = () => {
+//     sheets.getData(rows => {
+//         rows.forEach((row) => {
+//             console.log(row)
+//             const li = document.createElement('li')
+//             li.innerHTML = row.user + ': ' + row.skills + ':::' + row.location + ':::'
+//             document.getElementById('results').appendChild(li)
+//         })
+//     })
+// }
+    //filter function now needed to only return arrays that include the search 'term'
+
+
+
+// functions below relate to the filter exercises in homework 05...
+//
+
 // const getData = () => {
 //
 //     const term = document.getElementById('term').value
@@ -68,4 +117,22 @@ const doMatch = () => {
 //             })
 //
 //         })
+// }
+
+//
+//
+// const term = document.getElementById('term').value
+//
+//
+//     const userMatch= (term) => {
+//         /*
+//         If the string contains search term, it should return the string.
+//         If it doesn't contain search term, it should return nothing
+//         */
+//         if (doMatch.indexOf('term') >= 0) {
+//             return term
+//         } else {
+//             console.log ()
+//         }
+//     }
 // }
